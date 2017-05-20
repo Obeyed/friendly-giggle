a55f86da0573b2eb8e8f0ff58c60595382d89f09
Elias Obeid <ekobeid@gmail.com>
2017-05-20 09:29:25 +0400

    first attempt at exercise 2.1

--- homework/02/LogAnalysis.hs
+++ homework/02/LogAnalysis.hs
@@ -1,4 +1,33 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
+
 import Log
 
+{- Second homework http://www.seas.upenn.edu/~cis194/spring13/hw/02-ADTs.pdf -}
+
+-- Exercise 1
+
+parseMessage :: String -> LogMessage
+parseMessage ('I':' ':xs) = LogMessage Info ts msg
+  where ts  = read (getNextWord xs) :: Int
+        msg = dropNextWord xs
+parseMessage ('W':' ':xs) = LogMessage Warning ts msg
+  where ts  = read (getNextWord xs) :: Int
+        msg = dropNextWord xs
+parseMessage ('E':' ':xs) = LogMessage (Error lvl) ts msg
+  where lvl = read (getNextWord xs) :: Int
+        ts  = read (getNextWord (dropNextWord xs)) :: Int
+        msg = dropNextWord $ dropNextWord xs
+parseMessage xs = Unknown xs
+
+getNextWord :: String -> String
+getNextWord []         = []
+getNextWord (x:" ")    = [x]
+getNextWord (x:' ':_)  = [x]
+getNextWord (x:xs)     = [x] ++ getNextWord xs
+
+dropNextWord :: String -> String
+dropNextWord [] = []
+dropNextWord (_:" ") = []
+dropNextWord (_:' ':xs) = xs
+dropNextWord (_:xs) = dropNextWord xs
