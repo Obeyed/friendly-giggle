61674203f7de5f54db30176751aa662371459bb8
Elias Obeid <ekobeid@gmail.com>
2017-07-22 17:30:12 +0400

    type classes ftw

--- homework/05/Calc.hs
+++ homework/05/Calc.hs
@@ -22,3 +22,28 @@ evalStr :: String -> Maybe Integer
 evalStr s = maybeEval (parseExp Lit Add Mul s)
   where maybeEval (Just a) = Just (eval a)
         maybeEval Nothing  = Nothing
+
+
+-- Exercise 3
+{-
+ - Create a type class called Expr with three methods called
+ - lit, add, and mul which parallel the constructors of ExprT.
+ -}
+class Expr a where
+  add, mul :: a -> a -> a
+  lit :: Integer -> a
+
+instance Expr ExprT where
+  lit a = Lit a
+  mul a b = Mul a b
+  add a b = Add a b
+
+instance Expr Integer where
+  lit a = a
+  mul a b = a * b
+  add a b = a + b
+
+-- constrain the type of the arguments to ExprT
+reify :: ExprT -> ExprT
+reify = id
+
